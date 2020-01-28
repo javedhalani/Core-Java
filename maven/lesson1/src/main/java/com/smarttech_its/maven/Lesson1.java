c76223cbedf9e6cff6db63c11366d29f8c752f00
jvdhalani <javedhalani@gmail.com>
2020-01-28 08:01:28 +0530

    Initial Commit for maven Folder

---
+++ maven/lesson1/src/main/java/com/smarttech_its/maven/Lesson1.java
@@ -0,0 +1,13 @@
+package com.smarttech_its.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class Lesson1 
+{
+    public static void main( final String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven/readme.md
@@ -0,0 +1,3 @@
+# To create a Basic Project use the following command
+
+mvn -B archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=com.smartttech-its.maven -DartifactId=lesson1
